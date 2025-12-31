e987f5e1aee2e54b1cb6d122b06f4b9e01fe8c94
ouranos56 <frinnovagen56bj@gmail.com>
2026-01-01 00:00:16 +0100

    Update fr_api.tsx

--- fr_frontend/app/fr_api.tsx
+++ fr_frontend/app/fr_api.tsx
@@ -1,6 +1,7 @@
 import axios from "axios"
 
 const fr_api = axios.create({
-    baseURL: process.env.NEXT_PUBLIC_FR_API_URL + "api/",
+    baseURL: `${process.env.NEXT_PUBLIC_FR_API_URL}/api`,
+    timeout: 30000,
 })
 export default fr_api
