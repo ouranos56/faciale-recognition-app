6edc52ceaf31ec3579f6e15d4bec26eb16c73aa3
ouranos56 <frinnovagen56bj@gmail.com>
2026-01-04 19:58:45 +0100

    Add files via upload

--- fr_frontend/app/api/send_mail/route.ts
+++ fr_frontend/app/api/send_mail/route.ts
@@ -1,5 +1,6 @@
 import { Resend } from "resend";
 import { NextResponse } from "next/server";
+export const runtime = "edge";
 
 const resend = new Resend(process.env.RESEND_API_KEY);
 
@@ -8,7 +9,7 @@ export async function POST(req: Request) {
     const { firstname, name, email, message, consented } = await req.json();
 
     const data = await resend.emails.send({
-      from: "Resend <onboarding@resend.dev>",
+      from: "FR app <onboarding@resend.dev>",
       to: ["frinnovagen56bj@gmail.com"],
       subject: "FR App – Nouveau message",
       html: `
