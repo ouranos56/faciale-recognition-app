40be99eed3c8780e7eab011291c47bcfb64bd236
ouranos56 <frinnovagen56bj@gmail.com>
2026-01-08 12:28:22 +0100

    Update views.py

--- backend/fr_api/views.py
+++ backend/fr_api/views.py
@@ -75,18 +75,6 @@ class RetrieveUpdateWrongPredictionView(generics.RetrieveUpdateDestroyAPIView):
     queryset = UploadImageAndPredict.objects.all()
     serializer_class = UploadImageAndPredictSerializer
     lookup_field = "id"
-        
-    # def get_object(self):
-    #     id = self.kwargs.get("id")
-    #     sid = self.kwargs.get("session_id")
-
-    #     if id:
-    #         return UploadImageAndPredict.objects.get(id=id)
-
-        if sid:
-            return UploadImageAndPredict.objects.get(session_id=sid)
-
-    #     raise NotFound("id ou session_id requis")
          
 
 class RetrieveUserUpdateFeedBackView(APIView):
