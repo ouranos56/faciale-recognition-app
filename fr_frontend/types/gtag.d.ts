56e3aa6e8c5d150b6a06b4218d0a5ebd50978b6a
ouranos56 <frinnovagen56bj@gmail.com>
2026-01-08 02:52:05 +0100

    Add files via upload

---
+++ fr_frontend/types/gtag.d.ts
@@ -0,0 +1,14 @@
+export {};
+
+declare global {
+  interface Window {
+    gtag: (
+      command: 'config' | 'event' | 'js',
+      targetId: string,
+      config?: {
+        page_path?: string;
+        [key: string]: unknown;
+      }
+    ) => void;
+  }
+}
