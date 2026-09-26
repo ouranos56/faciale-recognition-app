4026ce41adba80ac4aac6c12b3cc7d42b5e49c45
ouranos56 <frinnovagen56bj@gmail.com>
2026-09-26 05:53:30 +0100

    Refactor ClientContent component and cleanup imports

--- fr_frontend/app/get-started/ClientContent.tsx
+++ fr_frontend/app/get-started/ClientContent.tsx
@@ -365,9 +365,10 @@ export default function ClientContent() {
   
   return (
     <>
+      <div className=" right-1/2 translate-x-1/2 w-[50vw] max-md:w-[85vw] text-amber-400/80 text-[18px] text-center bg-amber-400/8 backdrop-blur-[2px] rounded-xl p-2 " >La reconnaissance d’image est temporairement indisponible pour des raisons financières liées au paiement de l’hébergeur.</div>
       <div className={` relative w-[75vw] md_body px48 bg-[#f7f5f35b] flex justify-between items-center md:gap-12 ${frpredictions.length !== 0 || upLoadedImages.length !== 0 ? "min-h-screen py-20" : "py-6 h-svh "} `}>
 
-      <div className=" absolute top-0 right-1/2 translate-x-1/2 w-[50vw] max-md:w-[85vw] text-amber-400/80 text-[18px] text-center bg-amber-400/8 backdrop-blur-[2px] rounded-xl p-2 " >La reconnaissance d’image est temporairement indisponible pour des raisons financières liées au paiement de l’hébergeur.</div>
+      {/* <div className=" absolute top-0 right-1/2 translate-x-1/2 w-[50vw] max-md:w-[85vw] text-amber-400/80 text-[18px] text-center bg-amber-400/8 backdrop-blur-[2px] rounded-xl p-2 " >La reconnaissance d’image est temporairement indisponible pour des raisons financières liées au paiement de l’hébergeur.</div> */}
         
         <FeedBacK showFeedBack= {showFeedBack} />
 
